cc5a78a29f47ea66578958d23a68b55192c7aed2
azavada <andrii.zavada@gmail.com>
2016-02-17 23:09:50 +0200

    added search request

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,9 +1,9 @@
-$(function() {
+$(function () {
     function addAutoComplete(selector) {
         var options = {
             source: function (request, response) {
                 $.ajax({
-                    url: 'api/station/' + encodeURIComponent(request.term)
+                    url: 'api/station/' + request.term
                 }).done(function (data) {
                     var result = [];
                     data.forEach(function (element) {
@@ -22,7 +22,7 @@ $(function() {
             }
         };
 
-        function renderItem (ul, item) {
+        function renderItem(ul, item) {
             return $("<li>")
                 .attr("data-value", item.value)
                 .addClass("input-lg")
@@ -36,6 +36,40 @@ $(function() {
             ._renderItem = renderItem
     }
 
+    function addSearchButtonHandler() {
+        $("#search").on("click", function () {
+            var query = $.param({
+                from: $("#from").val(),
+                to: $("#to").val(),
+                when: getTodayDate()
+            });
+
+            $.ajax({
+                url: 'api/tickets?' + query
+            }).done(function (data) {
+                console.log(data);
+            });
+        });
+    }
+
+    function getTodayDate() {
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth() + 1; //January is 0!
+        var yyyy = today.getFullYear();
+
+        if (dd < 10) {
+            dd = '0' + dd
+        }
+
+        if (mm < 10) {
+            mm = '0' + mm
+        }
+
+        return dd + "." + mm + "." + yyyy;
+    }
+
     addAutoComplete("#from");
     addAutoComplete("#to");
+    addSearchButtonHandler();
 });
